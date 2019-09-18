158754b9167d0f88c9b671dd8662a266c077c638
JNLanthanide <jasper.neo@gmail.com>
2019-09-18 13:29:19 +0200

    Added DetermineFileType and fixed test.sh

--- file.c
+++ file.c
@@ -39,6 +39,36 @@ int print_error(char* path, int errnum) {
     path, strerror(errnum));
 }
 
+void DetermineFileType(FILE* f, char* path) {
+  int buffer = 0;
+
+  int i = 0;
+
+  int isASCII = 1;
+
+  while(1) {
+    if (fread(&buffer, 1, 1, f) != 0) {
+      if (!(buffer >= 0x07 && buffer <= 0x0D) && (buffer != 0x1B) && !(buffer >= 0x20 && buffer <= 0x7E)) {
+        isASCII = 0;
+      }
+    }
+    else {
+      if (i == 0) {
+        print_result(path, EMPTY);
+      } else {
+        if (isASCII == 1) {
+          print_result(path, ASCII);
+        }
+        else {
+          print_result(path, DATA);
+        }
+      }
+      break;
+    }
+    i++;
+  }
+}
+
 int main(int argc, char *argv[])
 {
   int retval = EXIT_SUCCESS;
@@ -57,32 +87,7 @@ int main(int argc, char *argv[])
     return retval;
   }
 
-  int buffer = 0;
+  DetermineFileType(f, argv[1]);
 
-  int i = 0;
-
-  int isASCII = 1;
-
-  while(1) {
-    if (fread(&buffer, 1, 1, f) != 0) {
-      if (!(buffer >= 0x07 && buffer <= 0x0D) && (buffer != 0x1B) && !(buffer >= 0x20 && buffer <= 0x7E)) {
-        isASCII = 0;
-      }
-    }
-    else {
-      if (i == 0) {
-        print_result(argv[1], EMPTY);
-      } else {
-        if (isASCII == 1) {
-          print_result(argv[1], ASCII);
-        }
-        else {
-          print_result(argv[1], DATA);
-        }
-      }
-      break;
-    }
-    i++;
-  }
   return retval;
 }
